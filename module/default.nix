554277eb76b8741f55dbd371ecb46c3fb0bea7ee
Silvan Schmidt <mail@silvanschmidt.ch>
2025-02-15 20:56:57 +0100

    init

---
+++ module/default.nix
@@ -0,0 +1,18 @@
+{ systemInformation, ... }:
+{
+  home-manager.users."${systemInformation.userName}" = {
+    programs.fish.enable = true;
+
+    programs.lsd.enable = true;
+    programs.fish.shellAbbrs.ls = "lsd";
+
+    programs.zoxide.enable = true;
+    programs.fish = {
+      interactiveShellInit = "zoxide init fish | source";
+      shellAbbrs.cd = "z";
+    };
+
+    programs.bat.enable = true;
+    programs.fish.shellAbbrs.cat = "bat";
+  };
+}
