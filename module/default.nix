f62e6914ada3fec9914eb76bfbefdb98981d52de
Silvan Schmidt <mail@silvanschmidt.ch>
2025-02-16 21:17:27 +0100

    feat: add starship

--- module/default.nix
+++ module/default.nix
@@ -1,4 +1,5 @@
-{ systemInformation, ... }:
+{ lib, systemInformation, ... }:
+
 {
   home-manager.users."${systemInformation.userName}" = {
     programs.fish.enable = true;
@@ -7,12 +8,35 @@
     programs.fish.shellAbbrs.ls = "lsd";
 
     programs.zoxide.enable = true;
-    programs.fish = {
-      interactiveShellInit = "zoxide init fish | source";
-      shellAbbrs.cd = "z";
-    };
+    programs.fish.shellAbbrs.cd = "z";
 
     programs.bat.enable = true;
     programs.fish.shellAbbrs.cat = "bat";
+
+    programs.starship = {
+      enable = true;
+      settings = {
+        format = lib.concatStrings [
+          "$battery"
+          "$directory"
+          "$character"
+        ];
+
+        right_format = lib.concatStrings [
+          "$git_branch"
+          "$git_status"
+        ];
+
+        character = {
+          success_symbol = "[~>](bold green)";
+          error_symbol = "[~>](bold red)";
+        };
+      };
+    };
+
+    programs.fish.interactiveShellInit = ''
+      zoxide init fish | source
+      starship init fish | source
+    '';
   };
 }
